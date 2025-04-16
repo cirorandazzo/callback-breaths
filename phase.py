ed6a824a37f92d2a0803b280c1b01ab06c6bcee7
Ciro Randazzo <ciro.randazzo@ucsf.edu>
2025-04-16 16:28:32 -0700

    refactor plot_duration_distribution into utils

--- phase.py
+++ phase.py
@@ -15,6 +15,7 @@ from matplotlib.backends.backend_pdf import PdfPages
 from utils.breath import (
     get_kde_distribution,
     get_phase,
+    plot_duration_distribution,
 )
 from utils.file import parse_birdname
 from utils.umap import (
@@ -87,43 +88,14 @@ no_call.type.value_counts()
 # %%
 # plot duration distributions and density etimation
 
-
-def plot_distrs(no_call):
-    fig, axs = plt.subplots(nrows=2, sharex=True)
-
-    for (type, breaths), ax in zip(no_call.groupby("type"), axs):
-        data = breaths["duration_s"]
-        title = f"{type} (n={len(data)})"
-
-        ax.hist(data, bins=np.linspace(0, 1.6, 200), label="data", density=True)
-        ax.set(title=title, ylabel="density")
-
-        kde, x_kde, y_kde = get_kde_distribution(data, xlim=(0, 1.6), xsteps=200)
-
-        ax.plot(
-            x_kde,
-            y_kde,
-            label="kde",
-        )
-
-        ax.axvline(x=np.mean(data), c="r", linewidth=0.5, linestyle="--", label="mean")
-
-    fig.tight_layout()
-
-    axs[0].legend()
-    ax.set(xlim=[-0.01, 0.6], xlabel="duration_s")
-
-    return fig, axs
-
-
 birds = no_call["birdname"].unique()
 
-fig, axs = plot_distrs(no_call)
+fig, axs = plot_duration_distribution(no_call)
 fig.suptitle(f"all birds (n={len(birds)})")
 fig.tight_layout()
 
 for bird in birds:
-    fig, axs = plot_distrs(no_call.loc[no_call["birdname"] == bird])
+    fig, axs = plot_duration_distribution(no_call.loc[no_call["birdname"] == bird])
     fig.suptitle(bird)
     fig.tight_layout()
 
