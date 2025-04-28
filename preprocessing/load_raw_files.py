53a8927ce939f2109a277e0fd0ca3536a98b2d71
Ciro Randazzo <ciro.randazzo@ucsf.edu>
2025-04-28 16:52:11 -0700

    reimplement preprocessing: standardized across experiments & multithreaded

--- preprocessing/load_raw_files.py
+++ preprocessing/load_raw_files.py
@@ -1,33 +1,144 @@
 # %%
 # load_raw_files.py
+#
+# each cell creates preprocessed npy files and metadata dataframes for the included paths
 
-import glob
+from pathlib import Path
 
 from utils.breath.preprocess import preprocess_files
 
+
+input_parent = Path(r"M:\ESZTER\BEHAVIOR\AIR SAC CALLS\HVC LESION\ASPIRATION")
+output_parent = Path(r"M:\randazzo\breathing\processed")
+
+# %%
 if __name__ == "__main__":
-    # Example usage
+    # %%
+    # CALLBACKS
 
-    cbin_files = [file for file in glob.glob("../data/test_input/cbin/*.cbin")]
-    preprocess_files(
-        files=cbin_files,
-        output_folder="../data/test_output/CBIN",
-        prefix="TEST_CBIN",
-        fs=32000,
-        channel_map={"audio": 0, "breath": 1},
-        has_stims=False,
-        stim_length=0.1,
-        output_exist_ok=True,  # False,
-    )
+    paths = [
+        input_parent.joinpath(p)
+        for p in [
+            # rd57rd97
+            "rd57rd97/preLesion/callbacks/female_230117",
+            "rd57rd97/preLesion/callbacks/female_230118",
+            "rd57rd97/preLesion/callbacks/male_230117",  # original
+            "rd57rd97/preLesion/callbacks/male_230118",
+            "rd57rd97/preLesion/callbacks/male_230119",
+            # pk19br8 (all original)
+            "pk19br8/preLesion/callback/rand/male_230215",
+            "pk19br8/preLesion/callback/rand/male_230216",
+            "pk19br8/preLesion/callback/rand/male_230217",
+            # rd99rd72
+            "rd99rd72/preLesion/callbacks/rand/230215",  # original
+            "rd99rd72/preLesion/callbacks/rand/230216",
+        ]
+        # note:
+        #   - excludes iso. 2 days/folders for each bird
+        #   - excludes bird rd56, with moving breath baseline
+    ]
 
-    wav_files = [file for file in glob.glob("../data/test_input/wav/*-B*.wav")]
+    files = [file for path in paths for file in path.glob("**/*-B*.wav")]
+
     preprocess_files(
-        files=wav_files,
-        output_folder="../data/test_output/WAV",
-        prefix="TEST_WAV",
+        files=files,
+        output_folder=output_parent.joinpath("callbacks"),
+        prefix="callback",
         fs=44100,
         channel_map={"audio": 0, "breath": 1, "stim": 2},
         has_stims=True,
         stim_length=0.1,
-        output_exist_ok=True,  # False,
+        output_exist_ok=False,
+    )
+
+    # %%
+    # SPONTANEOUS
+
+    paths = [
+        input_parent.joinpath(p)
+        for p in [
+            # callback birds
+            "rd57rd97/preLesion/spontaneous",
+            "pk19br8/preLesion/spontaneous",
+            "rd99rd72/preLesion/spontaneous",
+            # TODO: other birds?
+        ]
+    ]
+
+    files = [file for path in paths for file in path.glob("**/*.cbin")]
+
+    preprocess_files(
+        files=files,
+        output_folder=output_parent.joinpath("spontaneous"),
+        prefix="spontaneous",
+        fs=32000,
+        channel_map={"audio": 0, "breath": 1},
+        has_stims=False,
+        stim_length=0.1,
+        output_exist_ok=False,
+    )
+
+    assert False, "check hvc lesion cb sample rate"
+
+    # %%
+    # HVC LESION - CALLBACK
+    #
+    # TODO: pare these down to birds with complete lesion
+
+    input_parent = Path(r"M:\ESZTER\BEHAVIOR\AIR SAC CALLS\HVC LESION\ASPIRATION")
+
+    paths = [
+        input_parent.joinpath(p)
+        for p in [
+            # rd57rd97
+            "rd57rd97/postLesion/callback/random/male_230125",
+            "rd57rd97/postLesion/callback/random/male_230126",
+            "rd57rd97/postLesion/callback/random/male_230127",
+            # pk19br8
+            "pk19br8/postLesion/callback/rand/230302",
+            "pk19br8/postLesion/callback/rand/230303",
+            # rd99rd72
+            "rd99rd72/postLesion/callback/rand",
+        ]
+        # note:
+        #   - excludes iso. >=1 day for each bird
+    ]
+
+    preprocess_files(
+        files=files,
+        output_folder=output_parent.joinpath("hvc_lesion_callback"),
+        prefix="hvc_lesion_callback",
+        fs=44100,
+        channel_map={"audio": 0, "breath": 1, "stim": 2},
+        has_stims=True,
+        stim_length=0.1,
+        output_exist_ok=False,
+    )
+
+    # %%
+    # HVC LESION - SPONTANEOUS
+    #
+
+    paths = [
+        input_parent.joinpath(p)
+        for p in [
+            # callback birds
+            "rd57rd97/postLesion/spontaneous",
+            "pk19br8/postLesion/spontaneous",
+            "rd99rd72/postLesion/spontaneous",
+            # TODO: other birds?
+        ]
+    ]
+
+    assert False, "check hvc lesion spontaneous sample rate"
+
+    preprocess_files(
+        files=files,
+        output_folder=output_parent.joinpath("hvc_lesion_spontaneous"),
+        prefix="hvc_lesion_spontaneous",
+        fs=32000,
+        channel_map={"audio": 0, "breath": 1},
+        has_stims=False,
+        stim_length=0.1,
+        output_exist_ok=False,
     )
