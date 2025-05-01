c6923d714e84746a014651d7181a3c95be25d0c2
Ciro Randazzo <cirorandazzo13@gmail.com>
2025-05-01 16:43:10 -0700

    run multi dataset gridsearch

--- umap/umap-03.00-train_multi_dataset.py
+++ umap/umap-03.00-train_multi_dataset.py
@@ -1,8 +1,9 @@
 # %%
 # umap-03.00-train_multi_dataset.py
-# 
+#
 # Training one embedding with data from multiple datasets (callback & spont data in ctrl, hvc_lesion conditions)
 
+from itertools import product
 import os
 from pathlib import Path
 import pickle
@@ -20,147 +21,196 @@ from utils.umap import run_umap_gridsearch
 # %autoreload 1
 # %aimport utils.umap
 
-# %%
-# parameters
-
-# path to pickled traces dataframe
-traces_path = r"M:\randazzo\breathing\processed\insp_traces.pickle"
-
-# path to save embeddings
-embedding_path = r"M:\randazzo\breathing\embeddings"
-
-# save precompuated distance matrices
-# distance_matrices = Path(traces_path).parent
-
-# path to metadata dataframes
-file_format = r"M:\randazzo\breathing\processed\datasets\{dataset}\{dataset}-{file}.pickle"
-
-datasets = [
-    "callback",
-    "spontaneous",
-    "hvc_lesion_callback",
-    "hvc_lesion_spontaneous",
-]
-
-fs_dataset = {
-    "callback": 44100,
-    "spontaneous": 32000,
-    "hvc_lesion_callback": 44100,
-    "hvc_lesion_spontaneous": 32000,
-}
-
-# breath type
-breath_type = "insp"
-
 
 # %%
-# load metadata dfs
 
-all_files, all_breaths = load_datasets(datasets, file_format, fs_dataset=fs_dataset)
+if __name__ == "__main__":
+    # %%
+    # parameters
 
-# cut to only this breath type
-all_breaths = all_breaths.loc[all_breaths.type == breath_type]
+    program_start = time.time()
 
-# sort indices
-all_breaths.sort_index(inplace=True)
-all_files.sort_index(inplace=True)
+    print("Setting parameters...")
 
-all_breaths
+    # path to pickled traces dataframe
+    traces_path = r"M:\randazzo\breathing\processed\insp_traces.pickle"
 
-# %%
-# load traces df
-#
-# takes a while, big.
+    # path to save embeddings
+    embedding_path = r"M:\randazzo\breathing\embeddings"
 
-with open(traces_path, "rb") as f:
-    df_traces = pickle.load(f)
+    # save precompuated distance matrices
+    # distance_matrices = Path(traces_path).parent
 
-df_traces.sort_index(inplace=True)
+    # path to metadata dataframes
+    file_format = r"M:\randazzo\breathing\processed\{dataset}\{dataset}-{file}.pickle"
 
-df_traces
+    datasets = [
+        "callback",
+        "spontaneous",
+        "hvc_lesion_callback",
+        "hvc_lesion_spontaneous",
+    ]
 
-# %%
-# assert traces match metadata
+    fs_dataset = {
+        "callback": 44100,
+        "spontaneous": 32000,
+        "hvc_lesion_callback": 44100,
+        "hvc_lesion_spontaneous": 32000,
+    }
 
-assert len(all_breaths) == len(df_traces), f"Different lengths! all_breaths: {len(all_breaths)} | df_traces: {len(df_traces)}"
+    # breath type
+    breath_type = "insp"
 
-assert all(all_breaths.index == df_traces.index), "Indices don't match!"
 
-# %%
-# check for errors
-#
-# these files will have df_traces.data = np.nan
-#
-# these are unlikely if interpolated w/o padding; all that data should exist
-# (and my code never has bugs)
+    # %%
+    # load metadata dfs
 
-ii_error = df_traces.error.notna()
-errored = df_traces.loc[ii_error]
+    print("Loading metadata datasets...")
 
-print(f"{len(errored)} breaths had errors.")
+    all_files, all_breaths = load_datasets(datasets, file_format, fs_dataset=fs_dataset)
 
-assert sum(ii_error) == 0, "Deal with these errors before constructing UMAP train data below."
+    # cut to only this breath type
+    all_breaths = all_breaths.loc[all_breaths.type == breath_type]
 
-# %%
-# subsample
-#
-# Subsample data, stratified by dataset
+    # sort indices
+    all_breaths.sort_index(inplace=True)
+    all_files.sort_index(inplace=True)
 
-# take min dataset length from each dataset
-n_samples = min(all_breaths.dataset.value_counts())
+    print(f"Loaded! (Total time elapsed: {time.time() - program_start}s)\n")
 
-idx_subsampled = []
+    all_breaths
 
-for d in datasets:
-    # Get indices for the current dataset
-    ii_dataset = all_breaths.loc[all_breaths.dataset == d].index
+    # %%
+    # load traces df
+    #
+    # takes a while, big.
+
+    print("Loading traces...")
+
+    with open(traces_path, "rb") as f:
+        df_traces = pickle.load(f)
+
+    df_traces.sort_index(inplace=True)
+
+    print(f"Loaded! (Total time elapsed: {time.time() - program_start}s)\n")
+
+    df_traces
+
+    # %%
+    # assert traces match metadata
+
+    assert len(all_breaths) == len(df_traces), f"Different lengths! all_breaths: {len(all_breaths)} | df_traces: {len(df_traces)}"
+
+    assert all(all_breaths.index == df_traces.index), "Indices don't match!"
+
+    # %%
+    # check for errors
+    #
+    # these files will have df_traces.data = np.nan
+    #
+    # these are unlikely if interpolated w/o padding; all that data should exist
+    # (and my code never has bugs)
+
+    ii_error = df_traces.error.notna()
+    errored = df_traces.loc[ii_error]
+
+    print(f"{len(errored)} breaths had errors.")
+
+    assert sum(ii_error) == 0, "Deal with these errors before constructing UMAP train data below."
+
+    # %%
+    # subsample
+    #
+    # Subsample data, stratified by dataset
+
+    # take min dataset length from each dataset
+    n_samples = min(all_breaths.dataset.value_counts())
+
+    print(f"Subsampling to {n_samples} per dataset...")
+
+    idx_subsampled = []
+
+    seed = 141787999960910448216396417811445764406
+    rng = np.random.default_rng(seed=seed)
+
+    for d in datasets:
+        # Get indices for the current dataset
+        ii_dataset = all_breaths.loc[all_breaths.dataset == d].index
+
+        # Randomly sample indices from the current dataset
+        ii_sampled = rng.choice(ii_dataset, n_samples, replace=False)
+
+        # Append sampled indices
+        idx_subsampled.extend(ii_sampled)
+
+    print(idx_subsampled)
+
+    # %%
+    # create train data
+
+    data = np.stack(df_traces.loc[idx_subsampled, "data"])
+
+    print(f"Train data shape: {data.shape}")
+    print(f"Subsampled! (Total time elapsed: {time.time() - program_start}s)\n")
     
-    # Randomly sample indices from the current dataset
+    # %%
 
-    assert False, "seed np.random.choice!" # TODO: seed np.random.choice
+    print("Making kwargs for gridsearch...")
 
-    ii_sampled = np.random.choice(ii_dataset, n_samples, replace=False)  
+    exclude = [
+        dict(
+            n_neighbors=5,
+            min_dist=0.01,
+            metric="euclidean",
+        ),
+        dict(
+            n_neighbors=5,
+            min_dist=0.01,
+            metric="cosine",
+        ),
+    ]
+
+    umap_params = dict(
+        n_neighbors=[5, 10, 100, 500],
+        min_dist=[0.001, 0.01, 0.1, 0.5],
+        metric=["euclidean", "cosine"],
+    )
+
+    # note: "breath_col" hardcoded to "breath_interpolated"
+
+    # make parameter combinations
+    conditions = []
+    for condition in product(*umap_params.values()):
+        these_params = {k: v for k, v in zip(umap_params.keys(), condition)}
+
+        if these_params in exclude:
+            print(f"Skipping this parameter set: {these_params}")
+        else:
+            conditions.append(these_params)
+
+
+    print(f"{len(conditions)} models to be trained:")
+
+    print(f"Kwargs prepared! (Total time elapsed: {time.time() - program_start}s)\n")
+    conditions
+
+    # %%
+    # run umap gridsearch
+    # 70 min for 2
     
-    # Append sampled indices
-    idx_subsampled.extend(ii_sampled)
+    print("Training UMAP embeddings...")
 
-print(idx_subsampled)
+    errors = run_umap_gridsearch(data, conditions, embedding_path, "insp", True, False)
+    
+    print(f"Finished training UMAPs! {len(errors)} errors.")
+    print(f"Total time elapsed: {time.time() - program_start}s)\n")
 
-# %%
-# create train data
+    # %%
+    # make entire dataset
 
-data = np.stack(df_traces.loc[idx_subsampled, "data"])
+    # idx = df_traces.index
+    # data = np.stack(df_traces["data"])
 
-print(f"Train data shape: {data.shape}")
-# %%
+    # del df_traces  # save some memory
 
-conditions = [
-    dict(
-        n_neighbors=5,
-        min_dist=0.01,
-        metric="euclidean",
-    ),
-    dict(
-        n_neighbors=5,
-        min_dist=0.01,
-        metric="cosine",
-    ),
-]
-
-# %%
-# run umap gridsearch
-# 70 min for 2
-
-errors = run_umap_gridsearch(data, conditions, embedding_path, "insp", True, False)
-
-print(f"{len(errors)} errors.")
-
-# %%
-# make entire dataset
-
-idx = df_traces.index
-data = np.stack(df_traces["data"])
-
-del df_traces  # save some memory
-
-print(f"Train data shape: {data.shape}")
+    # print(f"Train data shape: {data.shape}")
