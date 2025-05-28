5da4a0356e5dac42f51ee0aa4d82fa5904d562d1
Ciro Randazzo <ciro.randazzo@ucsf.edu>
2025-05-28 10:36:39 -0700

    additional exploration of long calls

--- exploratory/long_calls.py
+++ exploratory/long_calls.py
@@ -15,6 +15,23 @@ from utils.breath.preprocess import load_datasets, TEMP_assert_file_quality
 from utils.breath.traces import cut_segment
 
 # %%
+# load long call data
+
+save_folder = Path(r"M:\randazzo\breathing\long_calls")
+
+# breaths - putative call only
+with open(save_folder / "thresholded_breaths-long_calls.pickle", "rb") as f:
+    thresholded = pickle.load(f)
+
+# files
+with open(save_folder / "all_files-long_calls.pickle", "rb") as f:
+    all_files = pickle.load(f)
+
+# all_breaths
+with open(save_folder / "all_breaths-long_calls.pickle", "rb") as f:
+    all_breaths = pickle.load(f)
+
+# %%
 # load data
 
 file_format = r"M:\randazzo\breathing\processed\{dataset}\{dataset}-{file}.pickle"
@@ -22,15 +39,15 @@ file_format = r"M:\randazzo\breathing\processed\{dataset}\{dataset}-{file}.pickl
 datasets = [
     "callback",
     "spontaneous",
-    # "hvc_lesion_callback",
-    # "hvc_lesion_spontaneous",
+    "hvc_lesion_callback",
+    "hvc_lesion_spontaneous",
 ]
 
 fs_dataset = {
     "callback": 44100,
     "spontaneous": 32000,
-    # "hvc_lesion_callback": 44100,
-    # "hvc_lesion_spontaneous": 32000,
+    "hvc_lesion_callback": 44100,
+    "hvc_lesion_spontaneous": 32000,
 }
 
 
@@ -50,12 +67,15 @@ file_cols = [
     "file_length_s",
     "resp_rate_file_avg",
     "breath_zero_point",
+    "dipstat",
 ]
 
 all_files = all_trials.xs(key=0, level="stims_index")[file_cols]
 
 # cut to only this breath type
-all_breaths = all_breaths.loc[all_breaths.type == "exp"]
+all_breaths = all_breaths.loc[
+    (all_breaths.type == "exp") & (np.isfinite(all_breaths.amplitude))
+]
 
 # sort indices
 all_breaths.sort_index(inplace=True)
@@ -106,6 +126,7 @@ for bird, df_bird in all_breaths.groupby(by="birdname"):
         ylim=(None, 4500),
     )
 
+ax.set(title="exp amplitude distr by bird")
 ax.legend()
 
 # %%
@@ -153,14 +174,15 @@ thresholded = all_breaths.loc[
 ]
 
 # some reports
+print("== POTENTIAL LONG CALLS: ")
 
 # number of expirations
 print(
     "{n_exps_thr} / {n_exps} expirations have duration >= {duration_threshold}s & amplitude >= {amplitude_threshold}.".format(
-        n_exps_thr =len(thresholded),
+        n_exps_thr=len(thresholded),
         n_exps=len(all_breaths),
-        duration_threshold = duration_threshold,
-        amplitude_threshold = amplitude_threshold,
+        duration_threshold=duration_threshold,
+        amplitude_threshold=amplitude_threshold,
     )
 )
 
@@ -173,7 +195,7 @@ print(
 )
 
 # breaths by bird
-print("Long call expirations by bird:")
+print("potential long call expirations by bird:")
 for k, v in thresholded.birdname.value_counts().items():
     print(f"\t- {k}\t{v}")
 
@@ -222,6 +244,11 @@ for i, (file, df_file) in enumerate(thresholded.groupby(by="numpy_filename")):
 # assert bool to ensure ~ operator works as expected
 thresholded.is_long_call = thresholded.is_long_call.astype(bool)
 
+# pass this data onto all_breaths
+all_breaths["is_long_call"] = thresholded["is_long_call"].reindex(
+    all_breaths.index, fill_value=False
+)
+
 # %%
 # long_calls_by_file = thresholded.groupby(by="numpy_filename").is_long_call.agg("sum")
 all_files["n_long_calls"] = thresholded.groupby(by="audio_filename").is_long_call.agg(
@@ -232,6 +259,23 @@ all_files.loc[all_files.n_long_calls.isna(), "n_long_calls"] = 0
 all_files["n_long_calls"] = all_files["n_long_calls"].astype(int)
 
 all_files.n_long_calls.sort_values(ascending=False)
+
+# %%
+# save long call data
+
+save_folder = Path(r"M:\randazzo\breathing\long_calls")
+
+# breaths - putative call only
+with open(save_folder / "thresholded_breaths-long_calls.pickle", "wb") as f:
+    pickle.dump(thresholded, f)
+
+# files
+with open(save_folder / "all_files-long_calls.pickle", "wb") as f:
+    pickle.dump(all_files, f)
+
+# all_breaths
+with open(save_folder / "all_breaths-long_calls.pickle", "wb") as f:
+    pickle.dump(all_breaths, f)
 
 # %%
 # n long calls per file
@@ -246,23 +290,24 @@ ax.set(
 )
 
 # %%
-#
+# long calls per file (by dataset)
 
 fig, ax = plt.subplots()
 for dset, dataset_df in all_files.groupby(by="dataset"):
     ax.hist(
         dataset_df.n_long_calls,
-        density=True,
         label=dset,
         # bins=np.linspace(-5000, 5000, 100),  # everything but outliers
         bins=np.arange(-0.5, 31.5, 1),
         histtype="step",
+        # density=True,
     )
 
 ax.set(
-    xlabel="no. long calls / file",
+    xlabel="long calls / file",
     ylabel="file count",
-    title="long calls by file (per dataset)",
+    # ylabel="density (files / dataset)",
+    title="long calls per file (by dataset)",
     xlim=[-1.5, 31.5],
 )
 
@@ -271,19 +316,34 @@ ax.legend()
 # %%
 # some useful plots
 
-# pick a file
-file = thresholded.index.get_level_values(0).unique()[17]
+# %%
+# pick a file & define plot functions
 
+# file = (
+#     thresholded.loc[
+#         (thresholded.dataset == "hvc_lesion_spontaneous") & (thresholded.is_long_call)
+#     ]
+#     .index.get_level_values(0)
+#     .unique()[153]
+# )
+file = r"M:\ESZTER\BEHAVIOR\AIR SAC CALLS\HVC LESION\ASPIRATION\pk19br8\postLesion\callback\rand\230302\pk19br820233217258-Block3.wav"
 
 # load data
 df_file = thresholded.xs(key=file, level=0)
+
+assert df_file.fs.nunique() == 1
+fs = df_file.iloc[0].fs
+
 data = np.load(df_file.numpy_filename.iloc[0])
+
 
 audio = data[audio_row, :]
 breath = data[breath_row, :]
 
 
 def plot_seg(row, ax, align=None, c=None):
+    fs = row.fs
+
     segment = cut_segment(
         breath,
         row,
@@ -300,7 +360,11 @@ def plot_seg(row, ax, align=None, c=None):
     ax.plot(x, segment, c=c)
 
 
-def plot_long_call_trace(ax, df_file, fs, breath, plot_seg):
+def plot_long_call_trace(ax, df_file, breath, plot_seg):
+
+    assert df_file.fs.nunique() == 1
+    fs = df_file.iloc[0].fs
+
     # plot trace
     ax.plot(np.arange(len(breath)) / fs, breath, c="k", linewidth=0.5)
 
@@ -316,15 +380,15 @@ def plot_long_call_trace(ax, df_file, fs, breath, plot_seg):
 
 
 # %%
-# plot breath trace for this file
+# plot breath trace for this file (highlight long calls)
 
-fig, ax = plt.subplots(figsize=(20, 15))
-plot_long_call_trace(ax, df_file, fs, breath, plot_seg)
+fig, ax = plt.subplots(figsize=(20, 10))
+plot_long_call_trace(ax, df_file, breath, plot_seg)
 
 # %%
-# plot spectrogram & breath trace for this file
-fig, axs = plt.subplots(nrows=2, sharex=True, figsize=(20, 15))
-axs[0].set(title=fstem, xlabel=None)
+# plot spectrogram & breath trace for this file (highlight long calls)
+fig, axs = plt.subplots(nrows=2, sharex=True, figsize=(20, 10))
+axs[0].set(title=Path(file).stem, xlabel=None)
 axs[1].set(xlabel="time (s)")
 
 # plot breath
@@ -335,7 +399,7 @@ ao.plot_spectrogram(ax=ax, cmap="jet", vmin=0.7)
 
 # plot breath trace (putative long calls highlighted)
 ax = axs[1]
-plot_long_call_trace(ax, df_file, fs, breath, plot_seg)
+plot_long_call_trace(ax, df_file, breath, plot_seg)
 
 # %%
 # plot 2 panel: long calls vs. non-long calls
@@ -348,3 +412,35 @@ ax.set(title="long call")
 ax = axs[1]
 df_file.loc[~df_file.is_long_call].apply(plot_seg, axis=1, args=[ax])
 ax.set(title="not long call")
+
+# %%
+# long call durations
+
+fig, ax = plt.subplots()
+
+ax.hist(
+    thresholded.loc[thresholded.is_long_call, "duration_s"],
+    bins=500,
+)
+
+# %%
+
+long_call_report = thresholded.groupby(by=["birdname", "dataset"]).agg(
+    n_long_calls=pd.NamedAgg(column="is_long_call", aggfunc=sum),
+    n_nonlong_calls=pd.NamedAgg(
+        column="is_long_call", aggfunc=lambda x: len(x) - sum(x)
+    ),
+    n_unique_files=pd.NamedAgg(column="numpy_filename", aggfunc=pd.Series.nunique),
+)
+
+long_call_report["long_calls_per_file"] = (
+    long_call_report.n_long_calls / long_call_report.n_unique_files
+)
+long_call_report["nonlong_calls_per_file"] = (
+    long_call_report.n_nonlong_calls / long_call_report.n_unique_files
+)
+long_call_report["long_to_nonlong"] = (
+    long_call_report.n_long_calls / long_call_report.n_nonlong_calls
+)
+
+long_call_report
